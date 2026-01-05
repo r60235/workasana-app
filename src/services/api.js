1dd8ae51c89f6e0049611bd20314ff628a318ba5
admin <admin-email@example.com>
2026-01-05 18:44:58 +0530

    deployed backend link added

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://workasana-backend-wine.vercel.app'; 
 
 // get token from localStorage
 const getToken = () => localStorage.getItem('workasana_token');
